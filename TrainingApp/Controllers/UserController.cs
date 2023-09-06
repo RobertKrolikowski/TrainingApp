c90e9961ebf8263c2fad47a102bbe1ade30d280b
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2023-09-06 23:04:39 +0200

    Test connect without entity framework

---
+++ TrainingApp/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+using TrainingApp.Model;
+
+namespace TrainingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT *
+	                    FROM public.""user""";
+            string sqlDataSource = _configuration.GetConnectionString("dockerDB");
+            NpgsqlDataReader reader;
+            List<User> users = new List<User>();
+            using (NpgsqlConnection connection = new NpgsqlConnection(sqlDataSource))
+            {
+                connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query,connection))
+                {
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        users.Add(new User()
+                        {
+                            ID = Int32.Parse(reader[0].ToString()),
+                            FirstName = reader[1].ToString(),
+                            LastName = reader[2].ToString()
+                        });
+
+                    }
+                    reader.Close();
+                    connection.Close();
+                }
+            }
+            return new JsonResult(users);
+        }
+    }
+}
---
+++ TrainingApp/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainingApp.Model
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
